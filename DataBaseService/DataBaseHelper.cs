fe11c2dbaa4508f2cc9df38f57c2aed83512dc3f
Jie1102 <jie.jie.1102@qq.com>
2017-07-06 10:10:04 +0800

    changed

--- DataBaseService/DataBaseHelper.cs
+++ DataBaseService/DataBaseHelper.cs
@@ -49,14 +49,15 @@ namespace DataBaseService
         /// <summary>
         /// 增删改查数据表
         /// </summary>
-        /// <param name="SQLString"></param>
-        /// <returns></returns>
+        /// <param name="SQLString">数据库操作语句</param>
+        /// <returns>执行结果</returns>
         public bool UpdateTable(string SQLString)
         {
             try
             {
                 MySqlCommand Command = new MySqlCommand(SQLString, Connection);
                 Command.ExecuteNonQuery();
+                Command.Dispose();
                 return true;
             }
             catch (Exception)
@@ -65,14 +66,41 @@ namespace DataBaseService
             }
         }
 
-        public void SelectTable(string SQLString)
+        /// <summary>
+        /// 查询数据库
+        /// </summary>
+        /// <param name="SQLString">查询语句</param>
+        /// <returns>保存查询结果的数据表</returns>
+        public DataTable SelectTable(string SQLString)
         {
+            DataTable Table = new DataTable();
 
+            try
+            {
+                MySqlDataAdapter Adapter = new MySqlDataAdapter(SQLString, Connection);
+                Adapter.Fill(Table);
+                return Table;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
+        /// <summary>
+        /// 关闭数据库连接
+        /// </summary>
         public void Dispose()
         {
-
+            try
+            {
+                Connection.Close();
+                Connection.Dispose();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
