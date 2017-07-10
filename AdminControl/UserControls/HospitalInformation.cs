09c05a5ac7a15f18f496132fa7df25d8fbb38255
Jie1102 <jie.jie.1102@qq.com>
2017-07-10 13:31:21 +0800

    changed

--- AdminControl/UserControls/HospitalInformation.cs
+++ AdminControl/UserControls/HospitalInformation.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 using EmailService;
 
 namespace AdminControl
@@ -41,11 +42,47 @@ namespace AdminControl
         }
 
         /// <summary>
+        /// BUG反馈
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_BugReport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
         /// 初始化
         /// </summary>
         private void InitItems()
         {
             Email = new EmailHelper();
+            ReadConfig();
+        }
+
+        /// <summary>
+        /// 读取配置
+        /// </summary>
+        private void ReadConfig()
+        {
+            StreamReader Reader;
+
+            string Config = null;
+
+            try
+            {
+                Reader = new StreamReader(Application.StartupPath + "\\Config\\HospitalInformationConfig.bin");
+                label_HospitalName.Text = Reader.ReadLine().Split(':')[1];
+
+                while ((Config = Reader.ReadLine()) != null)
+                {
+                    txt_ApplicationInformation.Text += Config + "\r\n";
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
