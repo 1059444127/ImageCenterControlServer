57fef9577cb7e8f0cb049794d337da068c5274be
Jie1102 <jie.jie.1102@qq.com>
2017-07-10 10:55:44 +0800

    changed

---
+++ EmailService/EmailHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailService
+{
+    /// <summary>
+    /// 邮件发送服务
+    /// </summary>
+    public class EmailHelper
+    {
+
+    }
+}
