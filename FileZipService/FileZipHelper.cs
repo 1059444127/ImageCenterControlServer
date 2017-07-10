07ac6cb6b3072d35eb33051f5b1d747d6582e1a8
Jie1102 <jie.jie.1102@qq.com>
2017-07-10 13:59:52 +0800

    changed

---
+++ FileZipService/FileZipHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ICSharpCode.SharpZipLib;
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Checksums;
+using ICSharpCode.SharpZipLib.Core;
+
+namespace FileZipService
+{
+    public class FileZipHelper
+    {
+        /// <summary>
+        /// 压缩文件
+        /// </summary>
+        /// <param name="sourceFilePath"></param>
+        /// <param name="destinationZipFilePath"></param>
+        public static void CreateZip(string sourceFilePath, string destinationZipFilePath)
+        {
+            if (sourceFilePath[sourceFilePath.Length - 1] != System.IO.Path.DirectorySeparatorChar)
+                sourceFilePath += System.IO.Path.DirectorySeparatorChar;
+
+            ZipOutputStream zipStream = new ZipOutputStream(File.Create(destinationZipFilePath));
+            zipStream.SetLevel(6);  // 压缩级别 0-9
+            CreateZipFiles(sourceFilePath, zipStream, sourceFilePath);
+
+            zipStream.Finish();
+            zipStream.Close();
+        }
+
+        /// <summary>
+        /// 递归压缩文件
+        /// </summary>
+        /// <param name="sourceFilePath">待压缩的文件或文件夹路径</param>
+        /// <param name="zipStream">打包结果的zip文件路径（类似 D:\WorkSpace\a.zip）,全路径包括文件名和.zip扩展名</param>
+        /// <param name="staticFile"></param>
+        private static void CreateZipFiles(string sourceFilePath, ZipOutputStream zipStream, string staticFile)
+        {
+            Crc32 crc = new Crc32();
+            string[] filesArray = Directory.GetFileSystemEntries(sourceFilePath);
+            foreach (string file in filesArray)
+            {
+                if (Directory.Exists(file))                     //如果当前是文件夹，递归
+                {
+                    CreateZipFiles(file, zipStream, staticFile);
+                }
+
+                else                                            //如果是文件，开始压缩
+                {
+                    FileStream fileStream = File.OpenRead(file);
+
+                    byte[] buffer = new byte[fileStream.Length];
+                    fileStream.Read(buffer, 0, buffer.Length);
+                    string tempFile = file.Substring(staticFile.LastIndexOf("\\") + 1);
+                    ZipEntry entry = new ZipEntry(tempFile);
+                    entry.DateTime = DateTime.Now;
+                    entry.Size = fileStream.Length;
+                    fileStream.Close();
+                    crc.Reset();
+                    crc.Update(buffer);
+                    entry.Crc = crc.Value;
+                    zipStream.PutNextEntry(entry);
+
+                    zipStream.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
