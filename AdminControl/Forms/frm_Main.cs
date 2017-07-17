bfec84f2638f6688d856c045bc4f4bb64bb12684
Jie1102 <jie.jie.1102@qq.com>
2017-07-17 13:52:09 +0800

    changed

--- AdminControl/Forms/frm_Main.cs
+++ AdminControl/Forms/frm_Main.cs
@@ -637,16 +637,16 @@ namespace AdminControl
                 //阅片室控制端连接
                 Log.WriteLog(string.Format("阅片室控制端{0}连接成功", ClientIP));
 
-                //启动传感器数据和灯光状态接收线程
-                YPOne.RecvDeviceDataThreadStart(UserConnection);
+                //启动数据接收线程
+
             }
             else if (ClientIP.Equals(ClientTypeConfig[1]))
             {
                 //阅片室客户端连接
                 Log.WriteLog(string.Format("阅片室客户端{0}连接成功", ClientIP));
 
-                //启动客户端指令接收线程
-                YPOne.RecvClientCommandThreadStart(UserConnection);
+                //启动指令接收线程
+
             }
             else if (ClientIP.Equals(ClientTypeConfig[2]))
             {
