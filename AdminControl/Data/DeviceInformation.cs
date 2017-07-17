94ed4d6b2a89a9e3a30911f4d1cb8348f58d0106
Jie1102 <jie.jie.1102@qq.com>
2017-07-17 09:09:12 +0800

    changed

---
+++ AdminControl/Data/DeviceInformation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminControl
+{
+    /// <summary>
+    /// 设备信息类
+    /// </summary>
+    class DeviceInformation
+    {
+        /// <summary>
+        /// 构造器
+        /// </summary>
+        /// <param name="ConfigPath"></param>
+        public DeviceInformation(string ConfigPath)
+        {
+
+        }
+
+        /// <summary>
+        /// 矩阵信息结构体
+        /// </summary>
+        public struct Mutrix
+        {
+            public string Name { get; set; }
+
+            public byte[] PowerOn { get; set; }
+
+            public byte[] PowerOff { get; set; }
+
+            public byte[] ChangeMode { get; set; }
+        }
+
+        /// <summary>
+        /// 投影机信息结构体
+        /// </summary>
+        public struct Projector
+        {
+            public string Name { get; set; }
+
+            public byte[] PowerOn { get; set; }
+
+            public byte[] PowerOff { get; set; }
+        }
+
+        /// <summary>
+        /// 镜头信息结构体
+        /// </summary>
+        public struct Camera
+        {
+            public string Name { get; set; }
+
+            public byte[] PowerOn { get; set; }
+
+            public byte[] PowerOff { get; set; }
+        }
+    }
+}
