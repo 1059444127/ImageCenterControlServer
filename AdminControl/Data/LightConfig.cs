0dc9cf763a5a975abe74e6218f572f7e30a3f5bb
Jie1102 <jie.jie.1102@qq.com>
2017-07-19 16:00:01 +0800

    changed

---
+++ AdminControl/Data/LightConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminControl
+{
+    class LightConfig
+    {
+        public string LightName { get; set; }
+
+        public string RelayNumber { get; set; }
+    }
+}
