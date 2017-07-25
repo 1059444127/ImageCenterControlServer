4c6575f992743c0df08ba16893d06efdd204e1a0
Jie1102 <jie.jie.1102@qq.com>
2017-07-25 14:35:40 +0800

    changed

--- AdminControl/UserControls/ReadingRoomOne.cs
+++ AdminControl/UserControls/ReadingRoomOne.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
-using System.Net;
 using System.Xml;
 using System.Net.Sockets;
 using DataTransferService;
