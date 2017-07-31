e959a373073e0b3997bb38f2f19cd9b994c73c86
Jie1102 <jie.jie.1102@qq.com>
2017-07-31 09:00:39 +0800

    changed

--- ReadingRoomClient/Resources/Resource.Designer.cs
+++ ReadingRoomClient/Resources/Resource.Designer.cs
@@ -63,8 +63,20 @@ namespace ReadingRoomClient
 			// aapt resource value: 0x7f050000
 			public const int btn_ConnectServer = 2131034112;
 			
+			// aapt resource value: 0x7f050005
+			public const int ib_ = 2131034117;
+			
+			// aapt resource value: 0x7f050002
+			public const int label_Hum = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int label_Light = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int label_Noise = 2131034116;
+			
 			// aapt resource value: 0x7f050001
-			public const int ib_ = 2131034113;
+			public const int label_Temp = 2131034113;
 			
 			static Id()
 			{
