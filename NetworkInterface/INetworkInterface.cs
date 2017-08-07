1940acb2b9eb0145230a03f836c46a3e2b381d39
Jie1102 <jie.jie.1102@qq.com>
2017-08-07 13:05:22 +0800

    changed

--- NetworkInterface/INetworkInterface.cs
+++ NetworkInterface/INetworkInterface.cs
@@ -7,10 +7,23 @@ using System.Net.Sockets;
 
 namespace NetworkInterface
 {
+    /// <summary>
+    /// 网络通讯接口
+    /// </summary>
     public interface INetworkInterface
     {
-        void SendData();
+        /// <summary>
+        /// 发送数据接口
+        /// </summary>
+        /// <param name="Connection">连接套接字</param>
+        /// <param name="Data">待发送的数据</param>
+        void SendData(Socket Connection, string Data);
 
-        string RecvData();
+        /// <summary>
+        /// 接收数据接口
+        /// </summary>
+        /// <param name="Connection">连接套接字</param>
+        /// <returns>接收到的数据</returns>
+        string RecvData(Socket Connection);
     }
 }
