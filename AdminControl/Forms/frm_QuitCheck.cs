412c2b1363ccdfee4b9e0dce6df4a38bfe9fe5ac
Jie1102 <jie.jie.1102@qq.com>
2017-08-21 16:23:42 +0800

    changed

--- AdminControl/Forms/frm_QuitCheck.cs
+++ AdminControl/Forms/frm_QuitCheck.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using DataHandleService;
+
 namespace AdminControl
 {
     /// <summary>
@@ -20,6 +22,11 @@ namespace AdminControl
         /// 主窗体实例
         /// </summary>
         private frm_Main frm_Main;
+
+        /// <summary>
+        /// 数据处理实例
+        /// </summary>
+        private DataHandleHelper Data;
         #endregion
 
         #region 构造器
@@ -52,7 +59,7 @@ namespace AdminControl
         /// <param name="e"></param>
         private void btn_QuitConfirm_Click(object sender, EventArgs e)
         {
-            CheckPasswd();
+            ConfirmQuit();
         }
 
         /// <summary>
@@ -74,26 +81,54 @@ namespace AdminControl
         private void InitItem(frm_Main frm_Main)
         {
             this.frm_Main = frm_Main;
+
+            Data = new DataHandleHelper();
         }
         #endregion
 
         #region 密码校验
         /// <summary>
-        /// 密码校验
+        /// 确认退出
         /// </summary>
-        public void CheckPasswd()
+        public void ConfirmQuit()
         {
             string Passwd = txt_AdminPasswd.Text;
-            if(string.IsNullOrEmpty(Passwd.Trim(' ')))
+
+            if (string.IsNullOrEmpty(Passwd.Trim(' ')))
             {
                 MessageBox.Show("密码不能为空", "错误");
+                return;
             }
-            else
+
+            if (CheckPasswd(Passwd))
             {
                 this.Hide();
                 frm_Main.CloseForm();
                 this.Close();
             }
+            else
+            {
+                MessageBox.Show("密码错误", "错误");
+            }
+        }
+
+        /// <summary>
+        /// 密码校验
+        /// </summary>
+        /// <param name="Passwd"></param>
+        /// <returns></returns>
+        protected bool CheckPasswd(string Passwd)
+        {
+            string SQLString = string.Format("select user_passwd from tb_userinformation where user_name = \"Admin\";");
+
+            DataTable Table = frm_Main.DataBase.SelectTable(SQLString);
+
+            if (Data.GetMD5String(Passwd).Equals(Table.Rows[0][0].ToString()))
+            {
+                return true;
+            }
+
+            return false;
         }
         #endregion
     }
