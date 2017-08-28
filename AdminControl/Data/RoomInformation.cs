71058a27c2658940c7bc9247777e4ab8040c9964
Jie1102 <jie.jie.1102@qq.com>
2017-08-28 08:22:46 +0800

    changed

---
+++ AdminControl/Data/RoomInformation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminControl
+{
+    /// <summary>
+    /// 房间类
+    /// </summary>
+    class RoomInformation
+    {
+        /// <summary>
+        /// 房间ID
+        /// </summary>
+        public int RoomID { get; set; }
+
+        /// <summary>
+        /// 房间名
+        /// </summary>
+        public string RoomName { get; set; }
+
+        /// <summary>
+        /// 房间使用状态
+        /// </summary>
+        public bool RoomStatus { get; set; }
+    }
+}
